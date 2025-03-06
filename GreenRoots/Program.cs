9f565b81be06f1e2e20f506f918a9aad718cd026
JoePapaya <johannestvedegaard@outlook.dk>
2025-03-06 13:59:30 +0100

    Færdig Program.cs
    Færdig version 2.0

--- GreenRoots/Program.cs
+++ GreenRoots/Program.cs
@@ -1,4 +1,6 @@
-﻿// Bambus Tandbørste
+// Definerer produkter og laver variabler til værdier som pris, antal solgte og kategori.
+
+// Bambus Tandbørste
 int produkt1Id = 1;
 string produkt1Navn = "Bambus tandbørste";
 string produkt1Kategori = "Bad";
@@ -55,24 +57,35 @@ Console.WriteLine(produkt5Navn + " har en omsætning på " + omsætning5 + " kr.
 
 
 
-static double BeregnOmsætning(double pris, int antalSolgt)
+
+static double BeregnOmsætning(double pris, int antalSolgt)                      // Opretter en metode til at udrenge omsætningen for hvert produkt. Dette gøres ved at vælge 2 inputs en double og en int som vi kalder pris og antalSolgt.
 {
-    return pris * antalSolgt;
+    return pris * antalSolgt;                                                   // Her defineres metoden til at gange pris med antalsolgte og returnere denne metode i BeregnOmsætning.
 }
-static string KategoriserOmsætning(double omsætning)
+                                                                                // Herefter kan vi kalde på metoden og indsætte prisen og antal solgte produkter for det specifikke produkt.
+
+                                                                                // Herefter kan vi kalde på metoden og indsætte prisen og antal solgte produkter for det specifikke produkt.
+
+                                                                                // Dette gemmes så i 5 double variabeler som vi navngiver omsætning 1 til 5.
+
+
+
+
+static string KategoriserOmsætning(double omsætning)                            // Vi opretter en metode til at kategorisere produktets omsætning.
 {
     {
-        if (omsætning < 10000)
+        if (omsætning < 10000)                                                  // Dette gøres med if else statements. Der laves krav for hver if statement.
         {
             return "Lav omsætning: under 10.000,-";
         }
-        else if (omsætning >= 10000 && omsætning <= 50000)
+        else if (omsætning >= 10000 && omsætning <= 50000)                      // Hvis omsætningen er mindre end 10.000 kr. Så returneres "Lav omsætning: under 10.000,-" 
+                                                                                // dette gøres for de andre krav også med omsætning mellem 10.000 og 50.000 samt over 50.000 kr.
         {
             return "Mellem omsætning: 10.000,- til 50.000,-";
         }
-        else
+        else                                                                    // Hvis ikke at omsætningen er under 10.000 eller mellem 10.000 og 50.000 kr. så bruger vi else til at udskrive "Omsætning over 50.000 kr.
         {
-            return "Høj omsætning: over 50.000,-";
+            return "Høj omsætning: over 50.000,-";                              // Metoden gemmes med return i (KategoriserOmsætning)
         }
 
     }
@@ -82,27 +95,30 @@ static string KategoriserOmsætning(double omsætning)
 int antalDage = 7;
 double totalOmsætning = 0;
 
-for (int dagNr = 1; dagNr <= antalDage; dagNr++)
+for (int dagNr = 1; dagNr <= antalDage; dagNr++)                                // Opretter et for loop der tæller fra dagNr = 1 til antalDage = 7 og laver en handling hver gang.
 
 {
 
-    // Simuler varierende dagligt salg ved hjælp af dagsnummeret
+                                                                                // Simuler varierende dagligt salg ved hjælp af dagsnummeret og modulo
 
-    int produkt1DagligtSalg = produkt1AntalSolgt / antalDage + (dagNr % 3);
-    int produkt2DaglitSalg = produkt2AntalSolgt / antalDage + (dagNr % 3);
-    int produkt3DaglitSalg = produkt3AntalSolgt / antalDage + (dagNr % 3);
+    int produkt1DagligtSalg = produkt1AntalSolgt / antalDage + (dagNr % 3);    // Bruger modulus til at give overblik over estimat for produkter solgt pr. dag. 
+    int produkt2DaglitSalg = produkt2AntalSolgt / antalDage + (dagNr % 3);     // Modulus bruges til at gøre salget mere varieret.
+    int produkt3DaglitSalg = produkt3AntalSolgt / antalDage + (dagNr % 3);     // Udregner produkter solgt dagligt med produktAntalSolgt / antalDage + (dagNr % 3)
     int produkt4DaglitSalg = produkt4AntalSolgt / antalDage + (dagNr % 3);
     int produkt5DaglitSalg = produkt5AntalSolgt / antalDage + (dagNr % 3);
 
-    // Beregn dagens omsætning
+   
     double dagsOmsætning = (produkt1DagligtSalg * produkt1Pris) + (produkt2DaglitSalg * produkt2Pris) + (produkt3DaglitSalg * produkt3Pris) +
         (produkt4DaglitSalg * produkt4Pris) + (produkt5DaglitSalg * produkt5Pris);
 
-    // Tilføj til totalOmsætning
+                                                                                // Summen af alle produkternes omsætning for dagen udregnes. Hver dag.
+   
     totalOmsætning = dagsOmsætning * antalDage;
-
-    // Udskriv dagens statistik
+                                                                                // Den totale omsætning for antalDage bliver udregnet
+   k
     Console.WriteLine("Dag nr. " + dagNr + "  er der blevet omsat for " + dagsOmsætning + " kr.");
 }
+                                                                                // Dagens statistik med dag nr. og dag omsætning bliver udskrevet hver dag.
 Console.WriteLine();
 Console.WriteLine("Totalomsætningen er " + totalOmsætning + " kr. i alt på " + antalDage + " dage");
+                                                                                // Uden for loopet bliver den totale omsætning for antalDage udskrevet.
